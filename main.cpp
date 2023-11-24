08f49784213a3ce32f1c66a80bc14318a01d02cb
Sergio Armesto <sergio.armesto@enti.cat>
2023-11-24 10:56:59 +0100

    Primer archivo en CPP

--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 
 
 int main (int argc, char** argv){
-
-//std::cout << argc << std::endl;
  
 if (argc > 1){
-std::cout << "Holi " << argv[1] << "!" << std::endl;
-}
-else{
+	std::cout << "Holi " << argv[1] << "!" << std::endl;
+	}
+	else{
 std::cout << "Introduce un nombre como parámetro!!!" << std::endl;
 }
 
